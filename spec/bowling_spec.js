7b787a830117e8d1f71d3ce679b0e21a067f2604
Andrew Snead <andrewcsnead@gmail.com>
2014-10-17 20:16:40 +0100

    add spare and strike rules

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -10,6 +10,11 @@ describe('Bowling Game', function() {
     this.roll(10)
   };
 
+  bowlSpare = function() {
+    this.roll(5)
+    this.roll(5)
+  };
+
   beforeEach(function() {
     game = new Game();
   });
@@ -30,14 +35,41 @@ describe('Bowling Game', function() {
     });
   });
 
+
+
   describe('Bowling a strike', function() {
 
-    it('should score 26 when strike is followed by 2 rolls of 4', function() {
+    it('should score a strike by itself as 10', function () {
+      bowlStrike.call(game);
+      multiRoll.call(game, 0, 18)
+      expect(game.score()).toEqual(10);
+    });
+
+    it('should add bonus score of 10 plus next 2 rolls', function() {
       bowlStrike.call(game);
       multiRoll.call(game, 4, 2);
       multiRoll.call(game, 0, 16);
       expect(game.score()).toEqual(26);
     });
+  });
+
+  describe('Bowling a spare', function() {
+    
+    it('should score a spare by itself as 10', function() {
+      bowlSpare.call(game);
+      multiRoll.call(game, 0, 18);
+      expect(game.score()).toEqual(10);
+    });
+
+    it('should add bonus score of 10 plus next roll', function() {
+      bowlSpare.call(game)
+      game.roll(5);
+      multiRoll.call(game, 0, 17);
+      expect(game.score()).toEqual(20);
+    });
+
+
+   
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -3,11 +3,35 @@ function Game() {
   this._current = 0;
 }
 
-Game.prototype.roll = function (pins) {
-  return this._rolls[this._current++] = pins;
+Game.prototype.roll = function(pins) {
+  this._rolls[this._current++] = pins;
 }
 
-Game.prototype.score = function () {
-  return this._rolls.reduce(function (a, b) {
-    return a + b; })
+Game.prototype.score = function() {
+  var score = 0;
+  for (var i = 0; i < this._rolls.length; i++) {
+    if (this._isStrike(i)) {
+      score += 10 + this._rolls[i+2] + this._rolls[i+1];
+    }
+    else if (this._isSpare(i)) {
+      score += 10 + this._rolls[i+2];
+      i++;
+    }
+    else {
+      score += this._rolls[i];
+    }
+  }
+  return score;
 };
+
+Game.prototype._isStrike = function(roll) {
+  if (this._rolls[roll] === 10) return true;
+  return false;
+}
+
+Game.prototype._isSpare = function(roll) {
+  if ((this._rolls[roll] % 2 !== 0 || roll === 0) && 
+    (this._rolls[roll] + this._rolls[roll+1] === 10)) return true;
+  return false;
+}
+
