4735dd4af0d4079a119773a7b401fd98d7aa5ab8
Andrew Snead <andrewcsnead@gmail.com>
2014-10-17 18:23:20 +0100

    modified score, roll methods

--- src/bowling.js
+++ src/bowling.js
@@ -1,11 +1,13 @@
 function Game() {
-  this._score = 0;
+  this._rolls = [];
+  this._current = 0;
 }
 
 Game.prototype.roll = function (pins) {
-  return this._score += pins;
+  return this._rolls[this._current++] = pins;
 }
 
 Game.prototype.score = function () {
-  return this._score;
+  return this._rolls.reduce(function (a, b) {
+    return a + b; })
 };
