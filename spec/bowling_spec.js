bd1a514dae6b2ef360ff01818a7b71a68101e936
Andrew Snead <andrewcsnead@gmail.com>
2014-10-17 16:39:40 +0100

    Added times function

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,12 +1,20 @@
 describe('Bowling Game', function () {
 
-  describe('Gutter for entire game', function () {
+  Number.prototype.times = function(args) { 
+    var args = Array.prototype.slice.call(arguments);
+    for(var i = 0; i < this; i++) {
+      args[0].apply(null, args.slice(1))
+    }
+  };
 
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  describe('Gutter for entire game', function () {
+   
     it('should be score 0 if no pins hit on all frames', function () {
-      var game = new Game();
-      for (var i = 0; i < 20; i++) {
-        game.roll(0);
-      }
+      (20).times(game.roll, 0);
       expect(game.score()).toEqual(0);
     });
   });
@@ -14,11 +22,11 @@ describe('Bowling Game', function () {
   describe('Two pins hit on every frame', function () {
 
     it('should be score 20', function () {
-      var game = new Game();
-      for(var i = 0; i < 20; i++) {
-        game.roll(1);
-      }
+      (20).times(game.roll, 1);
     });
 
   });
 });
+
+
+
